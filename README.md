db42ad914eed0a51c93a109cfea23da9be775f0a
Erin Cola <colaerin@yahoo.com>
2021-03-12 02:44:22 -0500

    changed readme and index.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Shop_to_Adopt_frontend
+
+I first need to create an index.html file, and make sure my index.js is linked to it in the html boilerplate script tag. 
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("in index.js");
+const PetsIndex = "http://localhost:3000/api/v1/pets"
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(PetsIndex)
+    .then(response => response.json())
+    .then(fosters => {
+        console.log(fosters);
+    })
+})
